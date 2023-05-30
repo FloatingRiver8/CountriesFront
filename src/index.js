ddfdb5c2c8c0e37e3afa3820d0ebe0b7943c8ec1
FloatingRiver8 <maceiramariana@gmail.com>
2023-05-30 13:09:58 -0300

    conecting to server

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {Provider} from 'react-redux';
 import {store} from './store'
 import axios from 'axios'
 
-  axios.defaults.baseURL = 'http://localhost:3001' //para local
-   /* axios.defaults.baseURL = 'https://countries-server-x977.onrender.com'   */
+/*   axios.defaults.baseURL = 'http://localhost:3001' */ //para local
+   axios.defaults.baseURL = 'https://countries-server-x977.onrender.com'  
 
  //Otros deploy del back por si no funciona la primera opción
  /* 'https://countries-back-gilt.vercel.app' */
